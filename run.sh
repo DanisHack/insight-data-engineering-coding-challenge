13e3eb6d1ceaa11d420932f18a6059eb455e2d24
Danish Mohd <danishmohd3610@gmail.com>
2015-07-12 08:21:43 +0530

    added run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# example of the run script for running the word count
+
+# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
+python analysis.py -i ../tweet_input/tweets.txt -w ../tweet_output/ft1.txt -m ../tweet_output/ft2.txt
