8252c18df39da4da1e80663da16ce07f5492b61d
Danish Mohd <danishmohd3610@gmail.com>
2015-07-12 08:27:53 +0530

    corrected paths

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python analysis.py -i ../tweet_input/tweets.txt -w ../tweet_output/ft1.txt -m ../tweet_output/ft2.txt
+python src/analysis.py -i tweet_input/tweet_input/tweets.txt -w tweet_output/tweet_output/ft1.txt -m tweet_output/tweet_output/ft2.txt
